4ee167536f2a1a14c8f2818b6c492048f3079062
StanleyXu <xutianlei@outlook.com>
2017-04-13 22:40:56 -0400

    fix plot function

--- R/export.heatmap.R
+++ R/export.heatmap.R
@@ -4,14 +4,14 @@
 #' @param mat query result from function query.egset.list()
 #' @param file path to the image file
 #' @param main title of the heatmap, default is ""
+#' @param silent bool; whether to show heatmap to current display device; default is T
 #' @keywords heatmap
 #' @export
 #' @examples
-#' export.heatmap(mat, file="result.heatmap.png", main="enrichment between tissue/pathway")
-export.heatmap=function(mat, file, main=""){
-  library(pheatmap)
-  library(RColorBrewer)
-
+#' result=query.egset.list(query.gr=query.gr, query.score=NULL, eqtl.set=eqtl.set.list, gene.set=biocarta, lr=F)
+#' mat=get.heat.mat(result, test.method = "fisher")
+#' export.heatmap(mat, file="result.heatmap.png", main="enrichment between tissue/pathway", silent=T)
+export.heatmap=function(mat, file, main="", silent=T){
   mat=-log(mat)
   mc=max(mat[-which(is.infinite(mat))]) + 100
   mat[which(is.infinite(mat))]=mc
@@ -25,6 +25,7 @@ export.heatmap=function(mat, file, main=""){
               cellwidth = 10, cellheight = 4, fontsize_row = 4, fontsize_col=6,
               breaks=breaks,color=color,
               legend=F,
-              main=main, filename = file)
+              main=main, filename = file,
+              silent=silent)
   #grid.text("ylabel example", x=0.5, rot=90, gp=gpar(fontsize=10))
 }
