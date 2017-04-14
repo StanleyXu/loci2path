42401db1c0ff49524f829428b2d4220d26f38fc0
StanleyXu <xutianlei@outlook.com>
2017-04-14 00:03:37 -0400

    doc data

--- R/egquery.demo-data.R
+++ R/egquery.demo-data.R
@@ -1,8 +1,13 @@
 #' eQTL geneset enrichment query demo data
 #'
-#' Data to show how to perform eQTL-geneset enrichment query. eQTL data are collected from GTeX.
-#' Geneset are from Broad Institute's MSigDB: curated gene set, category 'cp': BIOCARTA
-#' Query regions are from immunoBase,
+#' Demo Data to show how to perform eQTL-geneset enrichment query. 
+#' 
+#' 
+#' \code{eqtl.set.list} A list of eQTLset objects; eQTL data are collected from GTeX.
+#' 
+#' \code{biocarta} A Geneset object; Geneset are from Broad Institute's MSigDB: curated gene set, category 'cp': BIOCARTA
+#' 
+#' \code{query.gr} A Genomic Region object; Query regions are from immunoBase, crohn's disease.
 #'
 #' @docType data
 #'
@@ -10,7 +15,51 @@
 #'
 #' @keywords datasets
 #'
-#' @export
 #' @examples
 #' data(egquery.demo)
-"egquery.demo"
+#' @export
+"eqtl.set.list"
+
+
+#' eQTL geneset enrichment query demo data
+#'
+#' Demo Data to show how to perform eQTL-geneset enrichment query. 
+#' 
+#' 
+#' \code{eqtl.set.list} A list of eQTLset objects; eQTL data are collected from GTeX.
+#' 
+#' \code{biocarta} A Geneset object; Geneset are from Broad Institute's MSigDB: curated gene set, category 'cp': BIOCARTA
+#' 
+#' \code{query.gr} A Genomic Region object; Query regions are from immunoBase, crohn's disease.
+#'
+#' @docType data
+#'
+#' @usage data(egquery.demo)
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(egquery.demo)
+#' @export
+"biocarta"
+
+#' eQTL geneset enrichment query demo data
+#'
+#' Demo Data to show how to perform eQTL-geneset enrichment query. 
+#' 
+#' \code{eqtl.set.list} A list of eQTLset objects; eQTL data are collected from GTeX.
+#' 
+#' \code{biocarta} A Geneset object; Geneset are from Broad Institute's MSigDB: curated gene set, category 'cp': BIOCARTA
+#' 
+#' \code{query.gr} A Genomic Region object; Query regions are from immunoBase, crohn's disease.
+#'
+#' @docType data
+#'
+#' @usage data(egquery.demo)
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(egquery.demo)
+#' @export
+"query.gr"
