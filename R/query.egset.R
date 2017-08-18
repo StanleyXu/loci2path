d455dd84d757658cafb285385f158786c6f505fb
StanleyXu <xutianlei@outlook.com>
2017-08-17 21:44:57 -0400

    test vectorize fisher

--- R/query.egset.R
+++ R/query.egset.R
@@ -10,7 +10,7 @@
 #' @export
 #' @examples
 #' #to be added
-query.egset=function(query.gr, query.score, eqtl.set, gene.set, parallel=F, verbose=F){
+query.egset2=function(query.gr, query.score, eqtl.set, gene.set, parallel=F, verbose=F){
   ## check gene id compatibility
   comp=check.geneid(eqtl.set, gene.set)
   if(comp[3]==0){
@@ -18,7 +18,7 @@ query.egset=function(query.gr, query.score, eqtl.set, gene.set, parallel=F, verb
   }
   if(verbose){
     ## report query region info
-    cat(paste0(length(query.gr), " Query Region(s): ", sum(width(query.gr)),"bps in total\n"))
+    cat(paste0(length(query.gr), " Query Region(s): ", sum(width(reduce(query.gr))),"bps in total\n"))
     cat("--eQTL Set:\n")
     print(eqtl.set)
     cat("--gene Set:\n")
@@ -41,7 +41,7 @@ query.egset=function(query.gr, query.score, eqtl.set, gene.set, parallel=F, verb
       query.one.set(query.gr, query.score, eqtl.set, x, q.all, n.snp.t, gene.all, gene.q)})
   }else{
     res.list=lapply(gene.set@gene.set, FUN=function(x){
-      query.one.set(query.gr, query.score, eqtl.set, x, q.all, n.snp.t, gene.all, gene.q)})    
+      query.one.set2(query.gr, query.score, eqtl.set, x, q.all, n.snp.t, gene.all, gene.q)})    
   }
 
   ## remove NA
@@ -53,6 +53,13 @@ query.egset=function(query.gr, query.score, eqtl.set, gene.set, parallel=F, verb
     }
     ## organize result
     res=data.frame(names(res.list), tt, stringsAsFactors = F)
+    
+    pval.fisher=phyper(res$q.j-1, res$j.all, n.snp.t-res$j.all, res$q.all, lower.tail=F)
+    log.ratio=log((res$q.j/res$q.all)/(res$j.all/n.snp.t))
+    pval.fisher.gene=phyper(res$gene.jq-1, res$gene.j, gene.all-res$gene.j, res$gene.q, lower.tail = F)
+    log.ratio.gene=log((res$gene.jq/res$gene.q)/(res$gene.j/gene.all))
+    
+    res=cbind(res[,1:5], log.ratio, res[,6],pval.fisher, res[,7:10], log.ratio.gene, pval.fisher.gene)
     colnames(res)=c(
       "name_pthw",
       #"genes_pthw", #removed; duplicate with num_gene_set
@@ -63,14 +70,14 @@ query.egset=function(query.gr, query.score, eqtl.set, gene.set, parallel=F, verb
       "log_ratio",
       "pval_lr",
       "pval_fisher",
-      "pval_hypergeom",
-      "num_gene_set",#gene.j,  num of gene in current geneset
+      #"pval_hypergeom", #removed; redundant
+      "num_gene_set", #gene.j,  num of gene in current geneset
       "num_gene_query", # gene.q, # number of genes associated with SNPs overlapped by query region
       "num_gene_hit", #gene.jq, # number of genes hit
       "gene_hit", #gene.hit, # display hit gene ids
       "log_ratio_gene", #log.ratio.gene, # log ratio based on gene numbers
-      "pval_fisher_gene", #pval.fisher.gene,
-      "pval_hypergeom_gene" #pval.hypergeom.gene
+      "pval_fisher_gene" #pval.fisher.gene
+      #"pval_hypergeom_gene" #pval.hypergeom.gene #removed
     )
   }
 
